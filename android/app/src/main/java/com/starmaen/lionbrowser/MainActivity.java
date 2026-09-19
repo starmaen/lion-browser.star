273e1c926890b66d9d877319f8d9ab90f03483c8
starmaen <star1741965@gmail.com>
2026-09-19 23:18:07 +0300

    feat: add Capacitor Android project

---
+++ android/app/src/main/java/com/starmaen/lionbrowser/MainActivity.java
@@ -0,0 +1,5 @@
+package com.starmaen.lionbrowser;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.starmaen.lionbrowser',
+  appName: 'Lion Browser',
+  webDir: 'dist'
+};
+
+export default config;
